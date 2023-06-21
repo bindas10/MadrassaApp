bf83eed6f5d87daac31726c466c9d61684100d63
Bindas <bindasbro1010@gmail.com>
2023-06-20 22:44:42 -0700

    First xmls

---
+++ app/src/main/java/com/example/myapplication/AddStudent_form.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddStudent_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student_form);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+        ((Button) findViewById(R.id.Add_student)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,AddStudent_form.class);
+                startActivity(intent);
+            }
+        });
+        ((Button)findViewById(R.id.Display_student)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DisplayStudents.class);
+                startActivity(intent);
+            }
+        });
+        ((Button)findViewById(R.id.dailyTask)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DailyTaskForm.class);
+                startActivity(intent);
+            }
+        });
+        ((Button)findViewById(R.id.Search_student)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Search_Student.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
