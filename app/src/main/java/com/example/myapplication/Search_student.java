6bf28b99c807411bed676246e9b13ae52f972a4d
Bindas <bindasbro1010@gmail.com>
2023-06-22 05:45:39 -0700

    all java commit

--- app/src/main/java/com/example/myapplication/Search_student.java
+++ app/src/main/java/com/example/myapplication/Search_student.java
@@ -1,4 +1,26 @@
 package com.example.myapplication;
 
-public class Search_student {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Search_student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_student);
+        ((Button)findViewById(R.id.searchbtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Search_student.this,Show_Daily_Task.class);
+                intent.putExtra("id",((EditText)findViewById(R.id.search)).getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
 }
